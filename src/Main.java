257837c5823fb0ac47fb1807cc901314e3492969
Felix Langat <fexlangat@gmail.com>
2025-04-24 08:52:18 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("yoo");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Double num1;
+        Double num2;
+        char operator;
+        Double result = 0.00;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first num: ");
+        num1 = scanner.nextDouble();
+
+
+        System.out.println("Enter operator (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+
+        System.out.println("Enter first num2: ");
+        num2 = scanner.nextDouble();
+
+
+
+        switch (operator){
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> result = num1 / num2;
+            default -> {
+                System.out.println("Invalid operator.");
+                return;
+            }
         }
+
+        System.out.println("The results is: " + result);
+
+        scanner.close();
     }
 }
